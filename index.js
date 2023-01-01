0de274e452aef9474268d24d8310dc1987b64cd3
arifulgony <arifulgony@gmail.com>
2023-01-01 18:34:26 +0600

    jwt

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ariful.1up09ti.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-//jwt
+//..jwt
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
